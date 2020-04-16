765301ca2a1f03d081c52423acf7097be3901085
Maria Fernanda Garcia Monterroso <201800087@colsanjosee.edu.gt>
2020-04-16 12:29:10 -0600

    Se configuró...

---
+++ ventas.php
@@ -0,0 +1,15 @@
+<?php
+
+$user=array("usuario1", "usuario2", "usuario3");
+$pass=array(123,456,789);
+
+$estado=false;
+
+$nombre=$_POST['nombre'];
+$password=$_POSY['passw'];
+
+$variab=count($user);
+
+for($x=0;)
+
+?>
